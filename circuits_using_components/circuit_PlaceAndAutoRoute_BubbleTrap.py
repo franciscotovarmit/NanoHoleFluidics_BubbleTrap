eb6a3cb6f4b67a165fc8acbae744930b0e04f899
francisco.tovarlopez@rmit.edu.au <68582908+franciscotovarmit@users.noreply.github.com>
2021-06-09 15:45:31 +1000

    Add files via upload
    reservoir included
    supplement objects included

--- circuits_using_components/circuit_PlaceAndAutoRoute_BubbleTrap.py
+++ circuits_using_components/circuit_PlaceAndAutoRoute_BubbleTrap.py
@@ -1,13 +1,13 @@
-from microfluidics_ipkiss3.technology import * #comment when running main script, uncomment to debug geometry
+#from microfluidics_ipkiss3.technology import * #comment when running main script, uncomment to debug geometry
 from ipkiss3 import all as i3
 import microfluidics_ipkiss3.all as microfluidics
 import sys
 import numpy as np
-sys.path.append("/home/fran/PycharmProjects/ipkiss_projects_old/scripts2021/microfluidics-thach-comments-master")
+sys.path.append("/home/fran/PycharmProjects/ipkiss_projects_old/scripts2021/NanoHoleFluidics_BubbleTrap-Thach-comments")
 
 from functions.position_coordinates import generate_positions
 
-from components.bubble_trap_fluid import BubbleTrapFluid
+from components.bubble_trap import BubbleTrapFluid
 # PCell containing several traps
 
 class CircuitOfTraps(microfluidics.PlaceAndAutoRoute):
@@ -110,73 +110,3 @@ if __name__ == "__main__":
 
 
     myCircuiTrap_layout.visualize(annotate = True)
-    '''
-    s = myCircuiTrap_layout.get_channels_shapes()
-    size = myCircuiTrap_layout.size_info()
-    cells = myCircuiTrap_layout.get_cell()
-
-    #new_Shape = i3.Shape([(-350, 3800),(-3500, 1345),(-4700, 1345),(-4700, 1647),(-3500, 1647),(-350, 1800)])
-    #myCircuiTrap_layout = myCircuiTrap.Layout(channels_shapes=[None,None,None,None,
-    #                                                          new_Shape,None,None])
-    myCircuiTrap_layout.visualize(annotate = True)
-
-    #myCircuiTrap_layout.write_gdsii("myCircuiTrapTrapsJune2020.gds")
-
-    #generating STL and OF sim
-
-    from microfluidics_ipkiss3.pysimul.openfoam.openfoam_engine import *
-    from microfluidics_ipkiss3.pysimul.openfoam.turbulence_model import *
-    from microfluidics_ipkiss3.pysimul.openfoam.transport_model import *
-
-    # Control how the mesh is generated
-
-    snap_control = SnapControl(num_smooth_patch=1)
-    add_layer_control = AddLayerControl(final_layer_thickness=10, min_thickness=1, num_grow=10)
-    mesh_quality_control = MeshQualityControl(max_non_orthogonal=90)
-    mesh_control = MeshControl(mesh_size=20, feature_refinement_level=2,
-                               surface_refinement_level=(1, 2),
-                               refinement_regions=[(1, 2)],
-                               snap_mesh=True, snap_control=snap_control,
-                               add_layer=True, add_layer_control=add_layer_control,
-                               mesh_quality_control=mesh_quality_control)
-
-    # Transport and turbulence models
-
-    transport_model = NewtonianModel(kinematic_viscosity=0.00345 / 1050)
-    turbulence_model = LaminarModel()
-
-    # Create a list of properties required by simpleFoam solver including the transport property
-    # and a list of properties returned by turbulenace model
-
-    properties = [transport_model.get_model_property()] + turbulence_model.get_model_properties()
-
-    # Initialise the engine with simpleFoam solver
-
-    engine = OpenFoamEngine(mesh_control=mesh_control, solver='simpleFoam',
-                            properties=properties)
-
-    from microfluidics_ipkiss3.pysimul.runtime.basic import *
-
-    params = dict()
-    params["engine"] = engine
-    params["inlets"] = [FixedVelocityInlet(in_port_number=0, velocity=(0.0, 50.0e-3, 0)),
-                        #FixedVelocityInlet(in_port_number=1, velocity=(0.0, -30e-3, 0.0)),
-                        #FixedVelocityInlet(in_port_number=2, velocity=(0.0, 30e-3, 0.0))
-                         ]
-
-    window_si = SizeInfo(west=-260, east=260, south=-360, north=360)
-    #params["window_size_info"] = window_si
-
-    # 2D simulation
-    params["dimensions"] = 3
-
-    # Create and run simulation
-
-    from ipkiss.plugins.simulation import *
-
-    simul = myCircuiTrap_layout.create_simulation(simul_params=params)
-
-    # Start running simulation
-
-    simul.procedure.run(case_name='block2x4_roundTT', use_existing_mesh=False, interactive=True)
-    '''
